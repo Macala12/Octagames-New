01de255a4d4beb5374a52c20673cc800b3ea3639
Macala12 <macalaoma12@gmail.com>
2025-05-24 17:15:00 +0100

    update config

--- public/Assets/_js/config.js
+++ public/Assets/_js/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = 'https://octagames-new-production.up.railway.app';
 // const API_BASE_URL = 'http://localhost:3000';
-const API_BASE_URL = 'https://octagames.vercel.app';
+const API_BASE_URL = 'https://octagames.onrender.com';
